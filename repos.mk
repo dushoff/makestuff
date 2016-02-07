6b1685042c3dc5bbdccb0f7eed186bb7d6d9a0b0
Your Name <you@example.com>
2016-02-07 17:02:30 -0500

    Add WA to repos.mk

--- repos.mk
+++ repos.mk
@@ -1,8 +1,13 @@
 dushoff_github = https://github.com/dushoff
 theobio_github = https://github.com/mac-theobio
+outbreak_github = https://github.com/Outbreak-analysis
+
 
 $(gitroot)/Disease_data:
 	cd $(dir $@) && git clone $(theobio_github)/$(notdir $@).git
 
 $(gitroot)/makestuff:
 	cd $(dir $@) && git clone $(dushoff_github)/$(notdir $@).git
+
+$(gitroot)/WA_Ebola_Outbreak:
+	cd $(dir $@) && git clone $(outbreak_github)/$(notdir $@).git
