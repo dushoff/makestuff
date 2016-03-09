9d58defcd022fa60c62be2fc06bb23ad2e718725
Your Name <you@example.com>
2016-03-08 19:08:11 -0500

    Autocommit (makestuff) On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   forms.mk
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	commit.time

--- forms.mk
+++ forms.mk
@@ -1,3 +1,5 @@
+up_date:
+	/bin/rm -f date.txt
 
 date.txt: 
 	date +"%d %b %Y" > $@
@@ -5,8 +7,8 @@ date.txt:
 date.pdf: date.txt
 	pdfroff $< > $@
 
-date.png: date.txt
-	convert -crop 90x28+36+32 $< $@
+date.png: date.txt Makefile
+	convert -density 300 -crop 340x76+20+35 $< $@
 
 date.%.png: date.png
 	convert -scale $*% $< $@
@@ -14,7 +16,7 @@ date.%.png: date.png
 date.jpg: date.txt
 	convert -crop 90x28+36+32 $< $@
 
-date.%.jpg: date.png
+date.%.jpg: date.jpg
 	convert -scale $*% $< $@
 
 name.pdf: name.txt
@@ -23,8 +25,13 @@ name.pdf: name.txt
 name.%.png: name.png
 	convert -scale $*% $< $@
 
-sig.%.jpg: $(ldrop)/sig.jpg
+sig.%.jpg: sig.jpg
+	convert -scale $*% $< $@
+
+sig.%.png: sig.png
 	convert -scale $*% $< $@
 
 %.ppmed.png: %.pdf
 	convert -density 400x400 $< $@
+
+include $(ms)/RR/pdf.mk
