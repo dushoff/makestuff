00c518f38fafc9f1c9938ad145a325a4738a380c
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-08-19 16:43:33 -0400

    Autocommit (makestuff) On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   linkdirs.mk
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	commit.time
    	deps.mk

--- linkdirs.mk
+++ linkdirs.mk
@@ -1,6 +1,9 @@
 ## Rules for making and linking repo-based directories
 ## Would be nice to combine this with repos.mk; feels a bit repetitive
-gitdirs = Birth_death_models Disease_data Latent_incidence_fitting Exponential_figures SIR_model_family fitting_code hybrid_fitting SIR_simulations Generation_distributions WA_Ebola_Outbreak Ebola_sims
+## There's also something from the newtalk development named resources.mk
+## Should this have a JD-specific name? Should we break out rules that do shit from rules that say where things are?
+
+gitdirs = Birth_death_models Disease_data Latent_incidence_fitting Exponential_figures SIR_model_family fitting_code hybrid_fitting SIR_simulations Generation_distributions WA_Ebola_Outbreak Ebola_sims autorefs
 
 ## A directory that's already there might have its own local.mk, but if we make the directory, we want to control it
 $(gitdirs):
