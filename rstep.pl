9bf17bee1ed88bf880105c03d5413ce127d7dc09
Jonathan Dushoff <dushoff@mcmaster.ca>
2017-03-21 23:04:38 -0400

    Cleaner matching in rstep

--- rstep.pl
+++ rstep.pl
@@ -1,3 +1,7 @@
+## Horrible, wet code.
+## Should really have an input table, and a two-level hash
+## But I probably never will
+
 use strict;
 use 5.10.0;
 
@@ -14,7 +18,7 @@ my $f = <>;
 my (%sources);
 $f =~ s/^#.*//;
 $f =~ s/\n#.*//g;
-while ($f =~ s/\<source\s*\(\s*"(.*?)"//){
+while ($f =~ s/\bsource\s*\(\s*"(.*?)"//){
 	$sources{$1}=0;
 }
 if (%sources){
@@ -26,10 +30,36 @@ if (%sources){
 
 # Loads
 my (%loads);
-while ($f =~ s/\<read\s*\(\s*"(.*?)"//){
+$f =~ s/^#.*//;
+$f =~ s/\n#.*//g;
+while ($f =~ s/\bload\s*\(\s*"(.*?)"//){
 	$loads{$1}=0;
 }
 if (%loads){
 	say "$basename.rdeps: ", join " ", keys %loads;
+	say"";
+}
+
+# Reads
+my (%reads);
+$f =~ s/^#.*//;
+$f =~ s/\n#.*//g;
+while ($f =~ s/\bread\.\w+\s*\(\s*"(.*?)"//){
+	$reads{$1}=0;
+}
+if (%reads){
+	say "$basename.rdeps: ", join " ", keys %reads;
+	say"";
+}
+
+# Writes
+my (%writes);
+$f =~ s/^#.*//;
+$f =~ s/\n#.*//g;
+while ($f =~ s/\bwrite\.\w+\s*\(\s*"(.*?)"//){
+	$writes{$1}=0;
+}
+if (%writes){
+	say "join " ", keys %writes, ": $basename.Rout ;";
 	say"";
 }
