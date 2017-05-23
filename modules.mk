62b0cf9e4fb86ea2dfb098ed196a78c7253ecb4d
Jonathan Dushoff <dushoff@mcmaster.ca>
2017-05-23 10:51:08 -0400

    Insist on making submodule directory

--- modules.mk
+++ modules.mk
@@ -4,10 +4,10 @@ repodirs = $(dushoff_github) $(ICI3D)
 repofiles = $(repodirs:%=%/Makefile)
 
 $(dushoff_github):
-	git submodule add git@github.com:dushoff/$@.git
+	git submodule add git@github.com:dushoff/$@.git || mkdir $@
 
 $(ICI3D):
-	git submodule add git@github.com:ICI3D/$@.git
+	git submodule add git@github.com:ICI3D/$@.git || mkdir $@
 
 # Worried about infinite loops; will touch command help with time stamp?
 # Do I need a sleep?
