599efab661266495c324af7011afc2c97e8dfbda
Jonathan Dushoff <dushoff@mcmaster.ca>
2017-05-24 20:18:33 -0400

    Autocommit (makestuff)

--- modules.mk
+++ modules.mk
@@ -1,6 +1,6 @@
 include $(ms)/repos.def
 
-repodirs = $(dushoff_github) $(ICI3D)
+repodirs = $(dushoff_github) $(ICI3D) $(Bio3SS) $(theobio_group)
 repofiles = $(repodirs:%=%/Makefile)
 
 $(dushoff_github):
@@ -9,6 +9,12 @@ $(dushoff_github):
 $(ICI3D):
 	git submodule add git@github.com:ICI3D/$@.git || mkdir $@
 
+$(Bio3SS):
+	git submodule add git@github.com:Bio3SS/$@.git || mkdir $@
+
+$(theobio_group):
+	git submodule add git@github.com:mac-theobio/$@.git || mkdir $@
+
 # Worried about infinite loops; will touch command help with time stamp?
 # Do I need a sleep?
 $(repofiles): %/Makefile: %
@@ -16,3 +22,14 @@ $(repofiles): %/Makefile: %
 	git submodule update $<
 	touch $@
 
+## To make things in these directories;
+#### make the directory
+#### go there and make and touch
+maketouch = cd $$(dir $$@) && $$(MAKE) $$* && touch $$*
+define dirmake
+$(1)/%.mk: ;
+$(1)/%: $(1)
+	$(maketouch)
+endef
+
+$(foreach dir,$(repodirs),$(eval $(call dirmake,$(dir))))
