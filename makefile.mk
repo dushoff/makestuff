c65073ef2b1bbe20b909693c0eaf268a61ebff33
Jonathan Dushoff <dushoff@mcmaster.ca>
2017-05-25 13:58:18 -0400

    Modular new repos (In progress)

--- makefile.mk
+++ makefile.mk
@@ -1,8 +1,6 @@
 
-# make files
-
-Sources = Makefile .gitignore README.md stuff.mk LICENSE.md
-include stuff.mk
+Sources = Makefile .gitignore README.md sub.mk LICENSE.md
+include sub.mk
 # include $(ms)/perl.def
 
 ##################################################################
@@ -10,11 +8,6 @@ include stuff.mk
 ## Content
 
 ######################################################################
-
-### Makestuff
-
-## Change this name to download a new version of the makestuff directory
-# Makefile: start.makestuff
 
 -include $(ms)/git.mk
 -include $(ms)/visual.mk
