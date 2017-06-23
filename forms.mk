9d9ad4de0c0057e6fc99fa1e8a50b7bf230d157e
Jonathan Dushoff <dushoff@mcmaster.ca>
2017-06-23 17:31:41 -0400

    Autocommit (makestuff)

--- forms.mk
+++ forms.mk
@@ -14,7 +14,10 @@ up_date:
 	ps2pdf $< > $@
 
 date.pdf: date.txt
-	pdfroff $< > $@
+	pdfroff $< | cpdf -crop "0.9in 10.8in 0.9in 0.2in" -stdin -o $@ 
+
+date_%.pdf: date.pdf
+	cpdf -scale-page "$* $*" -o $@ $<
 
 date.png: date.pdf
 	$(imageconvert)
