8a44787de0ba7d8f028acdd3937f012ab8af4d6f
Jonathan Dushoff <dushoff@mcmaster.ca>
2017-08-17 14:37:39 -0400

    Autocommit (makestuff)

--- modules.mk
+++ modules.mk
@@ -2,7 +2,7 @@ include $(ms)/repos.def
 
 ## Add organizations to list, and make a rule
 
-repodirs = $(dushoff_github) $(ici3d_github) $(Bio3SS) $(theobio_group) $(outbreak_github)
+repodirs = $(dushoff_github) $(ici3d_github) $(Bio1M) $(Bio3SS) $(theobio_group) $(outbreak_github)
 
 repofiles = $(repodirs:%=%/Makefile)
 
