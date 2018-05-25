841675966befdc2d51716ccfb398e522bbc4bfba
Jonathan Dushoff <dushoff@mcmaster.ca>
2018-05-24 22:23:49 -0400

    Better error reporting, directory making?

--- webpix.mk
+++ webpix.mk
@@ -13,7 +13,7 @@ Ignore += $(steps:%=%.mk)
 	$(PUSH)
 
 Ignore += $(steps:.step=.html)
-%.html: %.step.mk $(ms)/webhtml.pl
+%.html: %.step.mk $(ms)/webhtml.pl webpix
 	$(MAKE) -f $< -f $(ms)/webtrans.mk images
 	$(MAKE) -f $< -f $(ms)/webtrans.mk thumbs
 	$(PUSHSTAR)
@@ -57,6 +57,6 @@ Makefile: allsteps.mk
 allsteps.mk: $(stepmks)
 	$(cat)
 
-webpix/%: allsteps.mk
+webpix/%: allsteps.mk webpix
 	$(MAKE) -f $< $@
 
