f82a960cdabed39b89e2df5ebbd30310b147ebeb
Jonathan Dushoff <dushoff@mcmaster.ca>
2018-06-20 15:28:29 -0400

    Working on Ignore stuff

--- autorefs.mk
+++ autorefs.mk
@@ -6,15 +6,23 @@ bibdir:
 export ms
 export autorefs = $(ms)/autorefs
 
+pmr = $(wildcard *.pmr)
 %.rmu: %.pmr
 	perl -wf $(autorefs)/nodoi.pl $< > $@
 
+pmrr = $(pmr:.pmr=.rmu)
+Ignore += $(pmrr)
+
+rmu = $(wildcard *.rmu) $(pmrr)
+Ignore += $(rmu:.rmu=.bib) $(rmu:.rmu=.md) $(rmu:.rmu=.html)
 %.bib %.int %.bibmk %.point: %.rmu
 	$(MAKE) -f $(autorefs)/Makefile $@
 
+Ignore += *.int *.ref *.refmk
 %.ref %.int %.refmk: %.rmu
 	$(MAKE) -f $(autorefs)/Makefile $@
 
+Ignore += *.mdmk
 %.md %.mdmk: %.rmu
 	$(MAKE) -f $(autorefs)/Makefile $@
 
