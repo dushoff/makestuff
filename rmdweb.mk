b5c4a8d43d1183cd8dd99258490715e66bfd5eb9
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-09-01 17:04:21 -0400

    Autocommit (makestuff)

--- rmdweb.mk
+++ rmdweb.mk
@@ -5,3 +5,14 @@
 mds_r = pandoc --mathjax -s -c main.css -B main.header.html -A main.footer.html -o $@ $<
 
 ## Source ⇒ product
+
+pages/%.html: %.mkd main.css main.header.html main.footer.html
+	$(mds_r)
+
+push_all:
+	cd pages && ! git commit -am "Orphan commit!"
+	cd pages && git pull || ( echo "WAIT: Don't push_all until you can pull" && false)
+	$(MAKE) $(pageProducts)
+	cd pages && git add $(pageProductsLocal) && git pull && git push
+
+all.time: push_all
