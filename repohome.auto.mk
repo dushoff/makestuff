aa2c7e61ef2bcebe93180d61f365ed7388083206
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-11-17 22:50:37 -0500

    ICI3D repos, sorting

--- repohome.auto.mk
+++ repohome.auto.mk
@@ -1,9 +1,9 @@
-rhdir/git_ICI3D_rubellaSA: url=https://github.com/ICI3D/rubellaSA.git
-rhdir/git_ICI3D_rubellaSA: ; $(rhmake)
 rhdir/bit_dushoff_Correspondence: url=https://bitbucket.org/dushoff/Correspondence.git
 rhdir/bit_dushoff_Correspondence: ; $(rhmake)
 rhdir/bit_dushoff_New_CV: url=https://bitbucket.org/dushoff/New_CV.git
 rhdir/bit_dushoff_New_CV: ; $(rhmake)
+rhdir/bit_dushoff_1m_private: url=https://dushoff@bitbucket.org/dushoff/1m_private.git
+rhdir/bit_dushoff_1m_private: ; $(rhmake)
 rhdir/bit_dushoff_Academic_CV: url=https://dushoff@bitbucket.org/dushoff/Academic_CV.git
 rhdir/bit_dushoff_Academic_CV: ; $(rhmake)
 rhdir/bit_dushoff_FIDO: url=https://dushoff@bitbucket.org/dushoff/FIDO.git
@@ -14,12 +14,68 @@ rhdir/bit_dushoff_nserc: url=https://dushoff@bitbucket.org/dushoff/nserc.git
 rhdir/bit_dushoff_nserc: ; $(rhmake)
 rhdir/bit_dushoff_qmee_private: url=https://dushoff@bitbucket.org/dushoff/qmee_private.git
 rhdir/bit_dushoff_qmee_private: ; $(rhmake)
+rhdir/git_bbolker_math1mp: url=https://github.com/bbolker/math1mp.git
+rhdir/git_bbolker_math1mp: ; $(rhmake)
 rhdir/git_bbolker_stat744: url=https://github.com/bbolker/stat744.git
 rhdir/git_bbolker_stat744: ; $(rhmake)
 rhdir/git_bbolker_stat744_private: url=https://github.com/bbolker/stat744_private.git
 rhdir/git_bbolker_stat744_private: ; $(rhmake)
+rhdir/git_Bio1M_Archive: url=https://github.com/Bio1M/Archive.git
+rhdir/git_Bio1M_Archive: ; $(rhmake)
+rhdir/git_Bio1M_Bio1M.github.io: url=https://github.com/Bio1M/Bio1M.github.io.git
+rhdir/git_Bio1M_Bio1M.github.io: ; $(rhmake)
+rhdir/git_Bio1M_Lectures_2017: url=https://github.com/Bio1M/Lectures_2017.git
+rhdir/git_Bio1M_Lectures_2017: ; $(rhmake)
+rhdir/git_Bio1M_Lectures: url=https://github.com/Bio1M/Lectures.git
+rhdir/git_Bio1M_Lectures: ; $(rhmake)
 rhdir/git_Bio3SS_3SS: url=https://github.com/Bio3SS/3SS.git
 rhdir/git_Bio3SS_3SS: ; $(rhmake)
+rhdir/git_Bio3SS_Age_distributions: url=https://github.com/Bio3SS/Age_distributions.git
+rhdir/git_Bio3SS_Age_distributions: ; $(rhmake)
+rhdir/git_Bio3SS_Assignments: url=https://github.com/Bio3SS/Assignments.git
+rhdir/git_Bio3SS_Assignments: ; $(rhmake)
+rhdir/git_Bio3SS_bio3ss3: url=https://github.com/Bio3SS/bio3ss3.git
+rhdir/git_Bio3SS_bio3ss3: ; $(rhmake)
+rhdir/git_Bio3SS_Bio3SS_content: url=https://github.com/Bio3SS/Bio3SS_content.git
+rhdir/git_Bio3SS_Bio3SS_content: ; $(rhmake)
+rhdir/git_Bio3SS_Bio3SS.github.io: url=https://github.com/Bio3SS/Bio3SS.github.io.git
+rhdir/git_Bio3SS_Bio3SS.github.io: ; $(rhmake)
+rhdir/git_Bio3SS_Birth_death_models: url=https://github.com/Bio3SS/Birth_death_models.git
+rhdir/git_Bio3SS_Birth_death_models: ; $(rhmake)
+rhdir/git_Bio3SS_Compensation_models: url=https://github.com/Bio3SS/Compensation_models.git
+rhdir/git_Bio3SS_Compensation_models: ; $(rhmake)
+rhdir/git_Bio3SS_Competition_models: url=https://github.com/Bio3SS/Competition_models.git
+rhdir/git_Bio3SS_Competition_models: ; $(rhmake)
+rhdir/git_Bio3SS_Evaluation_materials: url=https://github.com/Bio3SS/Evaluation_materials.git
+rhdir/git_Bio3SS_Evaluation_materials: ; $(rhmake)
+rhdir/git_Bio3SS_Exploitation_models: url=https://github.com/Bio3SS/Exploitation_models.git
+rhdir/git_Bio3SS_Exploitation_models: ; $(rhmake)
+rhdir/git_Bio3SS_Exponential_figures: url=https://github.com/Bio3SS/Exponential_figures.git
+rhdir/git_Bio3SS_Exponential_figures: ; $(rhmake)
+rhdir/git_Bio3SS_Grading: url=https://github.com/Bio3SS/Grading.git
+rhdir/git_Bio3SS_Grading: ; $(rhmake)
+rhdir/git_Bio3SS_Grading_scripts: url=https://github.com/Bio3SS/Grading_scripts.git
+rhdir/git_Bio3SS_Grading_scripts: ; $(rhmake)
+rhdir/git_Bio3SS_Homework: url=https://github.com/Bio3SS/Homework.git
+rhdir/git_Bio3SS_Homework: ; $(rhmake)
+rhdir/git_Bio3SS_Lecture_images: url=https://github.com/Bio3SS/Lecture_images.git
+rhdir/git_Bio3SS_Lecture_images: ; $(rhmake)
+rhdir/git_Bio3SS_Lectures: url=https://github.com/Bio3SS/Lectures.git
+rhdir/git_Bio3SS_Lectures: ; $(rhmake)
+rhdir/git_Bio3SS_Life_history: url=https://github.com/Bio3SS/Life_history.git
+rhdir/git_Bio3SS_Life_history: ; $(rhmake)
+rhdir/git_Bio3SS_Life_tables: url=https://github.com/Bio3SS/Life_tables.git
+rhdir/git_Bio3SS_Life_tables: ; $(rhmake)
+rhdir/git_Bio3SS_Population_time_series: url=https://github.com/Bio3SS/Population_time_series.git
+rhdir/git_Bio3SS_Population_time_series: ; $(rhmake)
+rhdir/git_Bio3SS_Structured_models: url=https://github.com/Bio3SS/Structured_models.git
+rhdir/git_Bio3SS_Structured_models: ; $(rhmake)
+rhdir/git_Bio3SS_submodLectures: url=https://github.com/Bio3SS/submodLectures.git
+rhdir/git_Bio3SS_submodLectures: ; $(rhmake)
+rhdir/git_Bio3SS_Teaching_team: url=https://github.com/Bio3SS/Teaching_team.git
+rhdir/git_Bio3SS_Teaching_team: ; $(rhmake)
+rhdir/git_Bio3SS_Tests: url=https://github.com/Bio3SS/Tests.git
+rhdir/git_Bio3SS_Tests: ; $(rhmake)
 rhdir/git_cfshi_cannabis: url=https://github.com/cfshi/cannabis.git
 rhdir/git_cfshi_cannabis: ; $(rhmake)
 rhdir/git_CYGUBICKO_admins: url=https://github.com/CYGUBICKO/admins.git
@@ -28,32 +84,44 @@ rhdir/git_cygubicko_funs: url=https://github.com/cygubicko/funs.git
 rhdir/git_cygubicko_funs: ; $(rhmake)
 rhdir/git_cygubicko_projects: url=https://github.com/cygubicko/projects.git
 rhdir/git_cygubicko_projects: ; $(rhmake)
+rhdir/git_CYGUBICKO_proview: url=https://github.com/CYGUBICKO/proview.git
+rhdir/git_CYGUBICKO_proview: ; $(rhmake)
 rhdir/git_cygubicko_wash: url=https://github.com/cygubicko/wash.git
 rhdir/git_cygubicko_wash: ; $(rhmake)
 rhdir/git_cygubicko_WDBC-Codes: url=https://github.com/cygubicko/WDBC-Codes.git
 rhdir/git_cygubicko_WDBC-Codes: ; $(rhmake)
+rhdir/git_davidearn_LBoM: url=https://github.com/davidearn/LBoM.git
+rhdir/git_davidearn_LBoM: ; $(rhmake)
+rhdir/git_davidearn_onmort: url=https://github.com/davidearn/onmort.git
+rhdir/git_davidearn_onmort: ; $(rhmake)
 rhdir/git_davidearn_plague: url=https://github.com/davidearn/plague.git
 rhdir/git_davidearn_plague: ; $(rhmake)
 rhdir/1M: url=https://github.com/dushoff/1M.git
 rhdir/1M: ; $(rhmake)
 rhdir/bio: url=https://github.com/dushoff/bio.git
 rhdir/bio: ; $(rhmake)
-rhdir/tp: url=https://github.com/dushoff/tp.git
-rhdir/tp: ; $(rhmake)
 rhdir/branches: url=https://github.com/dushoff/branches.git
 rhdir/branches: ; $(rhmake)
 rhdir/CIHR_Ebola: url=https://github.com/dushoff/CIHR_Ebola.git
 rhdir/CIHR_Ebola: ; $(rhmake)
 rhdir/Circumcision_and_behaviour: url=https://github.com/dushoff/Circumcision_and_behaviour.git
 rhdir/Circumcision_and_behaviour: ; $(rhmake)
+rhdir/cron: url=https://github.com/dushoff/cron.git
+rhdir/cron: ; $(rhmake)
 rhdir/disease_model_talks: url=https://github.com/dushoff/disease_model_talks.git
 rhdir/disease_model_talks: ; $(rhmake)
 rhdir/diversity_metrics: url=https://github.com/dushoff/diversity_metrics.git
 rhdir/diversity_metrics: ; $(rhmake)
 rhdir/dushoff.github.io: url=https://github.com/dushoff/dushoff.github.io.git
 rhdir/dushoff.github.io: ; $(rhmake)
+rhdir/Endemic_curves: url=https://github.com/dushoff/Endemic_curves.git
+rhdir/Endemic_curves: ; $(rhmake)
+rhdir/Generation_distributions: url=https://github.com/dushoff/Generation_distributions.git
+rhdir/Generation_distributions: ; $(rhmake)
 rhdir/generation_links: url=https://github.com/dushoff/generation_links.git
 rhdir/generation_links: ; $(rhmake)
+rhdir/Generation_talks: url=https://github.com/dushoff/Generation_talks.git
+rhdir/Generation_talks: ; $(rhmake)
 rhdir/Latent_incidence_fitting: url=https://github.com/dushoff/Latent_incidence_fitting.git
 rhdir/Latent_incidence_fitting: ; $(rhmake)
 rhdir/link_calculations: url=https://github.com/dushoff/link_calculations.git
@@ -72,12 +140,22 @@ rhdir/scratch: url=https://github.com/dushoff/scratch.git
 rhdir/scratch: ; $(rhmake)
 rhdir/significance: url=https://github.com/dushoff/significance.git
 rhdir/significance: ; $(rhmake)
+rhdir/SIR_model_family: url=https://github.com/dushoff/SIR_model_family.git
+rhdir/SIR_model_family: ; $(rhmake)
+rhdir/SIR_simulations: url=https://github.com/dushoff/SIR_simulations.git
+rhdir/SIR_simulations: ; $(rhmake)
+rhdir/ss_pix: url=https://github.com/dushoff/ss_pix.git
+rhdir/ss_pix: ; $(rhmake)
 rhdir/statistics_talks: url=https://github.com/dushoff/statistics_talks.git
 rhdir/statistics_talks: ; $(rhmake)
 rhdir/talks: url=https://github.com/dushoff/talks.git
 rhdir/talks: ; $(rhmake)
+rhdir/tp: url=https://github.com/dushoff/tp.git
+rhdir/tp: ; $(rhmake)
 rhdir/Workshops: url=https://github.com/dushoff/Workshops.git
 rhdir/Workshops: ; $(rhmake)
+rhdir/git_fishforwish_aging: url=https://github.com/fishforwish/aging.git
+rhdir/git_fishforwish_aging: ; $(rhmake)
 rhdir/git_fishforwish_fgc: url=https://github.com/fishforwish/fgc.git
 rhdir/git_fishforwish_fgc: ; $(rhmake)
 rhdir/git_ICI3D_coreFaculty: url=https://github.com/ICI3D/coreFaculty.git
@@ -88,12 +166,22 @@ rhdir/git_ICI3D_MMED: url=https://github.com/ICI3D/MMED.git
 rhdir/git_ICI3D_MMED: ; $(rhmake)
 rhdir/git_ICI3D_MMEDparticipants: url=https://github.com/ICI3D/MMEDparticipants.git
 rhdir/git_ICI3D_MMEDparticipants: ; $(rhmake)
+rhdir/git_ICI3D_rabies2019: url=https://github.com/ICI3D/rabies2019.git
+rhdir/git_ICI3D_rabies2019: ; $(rhmake)
 rhdir/git_ICI3D_RTutorials: url=https://github.com/ICI3D/RTutorials.git
 rhdir/git_ICI3D_RTutorials: ; $(rhmake)
+rhdir/git_ICI3D_rubellaSA: url=https://github.com/ICI3D/rubellaSA.git
+rhdir/git_ICI3D_rubellaSA: ; $(rhmake)
 rhdir/git_mac-theobio_ari_submission: url=https://github.com/mac-theobio/ari_submission.git
 rhdir/git_mac-theobio_ari_submission: ; $(rhmake)
 rhdir/git_mac-theobio_Condom_awareness: url=https://github.com/mac-theobio/Condom_awareness.git
 rhdir/git_mac-theobio_Condom_awareness: ; $(rhmake)
+rhdir/git_mac-theobio_DataViz: url=https://github.com/mac-theobio/DataViz.git
+rhdir/git_mac-theobio_DataViz: ; $(rhmake)
+rhdir/git_mac-theobio_DataViz_private: url=https://github.com/mac-theobio/DataViz_private.git
+rhdir/git_mac-theobio_DataViz_private: ; $(rhmake)
+rhdir/git_mac-theobio_Disease_data: url=https://github.com/mac-theobio/Disease_data.git
+rhdir/git_mac-theobio_Disease_data: ; $(rhmake)
 rhdir/git_mac-theobio_GOS_NMF: url=https://github.com/mac-theobio/GOS_NMF.git
 rhdir/git_mac-theobio_GOS_NMF: ; $(rhmake)
 rhdir/git_mac-theobio_HIV_treatment_Africa: url=https://github.com/mac-theobio/HIV_treatment_Africa.git
@@ -114,10 +202,12 @@ rhdir/git_mikeroswell_users_guide_clean: url=https://github.com/mikeroswell/user
 rhdir/git_mikeroswell_users_guide_clean: ; $(rhmake)
 rhdir/git_Outbreak-analysis_madagascar_tb_hiv: url=https://github.com/Outbreak-analysis/madagascar_tb_hiv.git
 rhdir/git_Outbreak-analysis_madagascar_tb_hiv: ; $(rhmake)
-rhdir/git_parksw3_bayes_antigen: url=https://github.com/parksw3/bayes_antigen.git
-rhdir/git_parksw3_bayes_antigen: ; $(rhmake)
+rhdir/git_Outbreak-analysis_WA_Ebola_Outbreak: url=https://github.com/Outbreak-analysis/WA_Ebola_Outbreak.git
+rhdir/git_Outbreak-analysis_WA_Ebola_Outbreak: ; $(rhmake)
 rhdir/git_parksw3_bayesantigen2: url=https://github.com/parksw3/bayesantigen2.git
 rhdir/git_parksw3_bayesantigen2: ; $(rhmake)
+rhdir/git_parksw3_bayes_antigen: url=https://github.com/parksw3/bayes_antigen.git
+rhdir/git_parksw3_bayes_antigen: ; $(rhmake)
 rhdir/git_parksw3_contact_trace: url=https://github.com/parksw3/contact_trace.git
 rhdir/git_parksw3_contact_trace: ; $(rhmake)
 rhdir/git_parksw3_latentGeneration: url=https://github.com/parksw3/latentGeneration.git
@@ -128,6 +218,10 @@ rhdir/git_parksw3_speedstrength: url=https://github.com/parksw3/speedstrength.gi
 rhdir/git_parksw3_speedstrength: ; $(rhmake)
 rhdir/git_PulliamLab-UFL_competenceFramework: url=https://github.com/PulliamLab-UFL/competenceFramework.git
 rhdir/git_PulliamLab-UFL_competenceFramework: ; $(rhmake)
+rhdir/git_SACEMA_epidemicInsurance: url=https://github.com/SACEMA/epidemicInsurance.git
+rhdir/git_SACEMA_epidemicInsurance: ; $(rhmake)
+rhdir/git_wzmli_glasgow: url=https://github.com/wzmli/glasgow.git
+rhdir/git_wzmli_glasgow: ; $(rhmake)
 rhdir/git_wzmli_ImpSamp: url=https://github.com/wzmli/ImpSamp.git
 rhdir/git_wzmli_ImpSamp: ; $(rhmake)
 rhdir/git_wzmli_impSamp: url=https://github.com/wzmli/impSamp.git
@@ -140,6 +234,8 @@ rhdir/git_wzmli_mli_thesis: url=https://github.com/wzmli/mli_thesis.git
 rhdir/git_wzmli_mli_thesis: ; $(rhmake)
 rhdir/git_wzmli_persistence: url=https://github.com/wzmli/persistence.git
 rhdir/git_wzmli_persistence: ; $(rhmake)
+rhdir/git_wzmli_phyloglmm: url=https://github.com/wzmli/phyloglmm.git
+rhdir/git_wzmli_phyloglmm: ; $(rhmake)
 rhdir/git_wzmli_rabies_cihr: url=https://github.com/wzmli/rabies_cihr.git
 rhdir/git_wzmli_rabies_cihr: ; $(rhmake)
 rhdir/git_wzmli_rabies_correlations: url=https://github.com/wzmli/rabies_correlations.git
@@ -152,99 +248,3 @@ rhdir/git_wzmli_rabies_report: url=https://github.com/wzmli/rabies_report.git
 rhdir/git_wzmli_rabies_report: ; $(rhmake)
 rhdir/git_wzmli_rdc: url=https://github.com/wzmli/rdc.git
 rhdir/git_wzmli_rdc: ; $(rhmake)
-rhdir/git_mac-theobio_DataViz: url=https://github.com/mac-theobio/DataViz.git
-rhdir/git_mac-theobio_DataViz: ; $(rhmake)
-rhdir/git_mac-theobio_DataViz_private: url=https://github.com/mac-theobio/DataViz_private.git
-rhdir/git_mac-theobio_DataViz_private: ; $(rhmake)
-rhdir/git_Bio1M_Lectures: url=https://github.com/Bio1M/Lectures.git
-rhdir/git_Bio1M_Lectures: ; $(rhmake)
-rhdir/git_Bio1M_Bio1M.github.io: url=https://github.com/Bio1M/Bio1M.github.io.git
-rhdir/git_Bio1M_Bio1M.github.io: ; $(rhmake)
-rhdir/git_Bio1M_Archive: url=https://github.com/Bio1M/Archive.git
-rhdir/git_Bio1M_Archive: ; $(rhmake)
-rhdir/git_Bio1M_Lectures_2017: url=https://github.com/Bio1M/Lectures_2017.git
-rhdir/git_Bio1M_Lectures_2017: ; $(rhmake)
-rhdir/git_bbolker_math1mp: url=https://github.com/bbolker/math1mp.git
-rhdir/git_bbolker_math1mp: ; $(rhmake)
-rhdir/git_wzmli_phyloglmm: url=https://github.com/wzmli/phyloglmm.git
-rhdir/git_wzmli_phyloglmm: ; $(rhmake)
-rhdir/git_SACEMA_epidemicInsurance: url=https://github.com/SACEMA/epidemicInsurance.git
-rhdir/git_SACEMA_epidemicInsurance: ; $(rhmake)
-rhdir/bit_dushoff_1m_private: url=https://dushoff@bitbucket.org/dushoff/1m_private.git
-rhdir/bit_dushoff_1m_private: ; $(rhmake)
-rhdir/git_Bio3SS_Bio3SS.github.io: url=https://github.com/Bio3SS/Bio3SS.github.io.git
-rhdir/git_Bio3SS_Bio3SS.github.io: ; $(rhmake)
-rhdir/git_Bio3SS_Grading: url=https://github.com/Bio3SS/Grading.git
-rhdir/git_Bio3SS_Grading: ; $(rhmake)
-rhdir/git_Bio3SS_Tests: url=https://github.com/Bio3SS/Tests.git
-rhdir/git_Bio3SS_Tests: ; $(rhmake)
-rhdir/git_Bio3SS_Assignments: url=https://github.com/Bio3SS/Assignments.git
-rhdir/git_Bio3SS_Assignments: ; $(rhmake)
-rhdir/git_Bio3SS_Exploitation_models: url=https://github.com/Bio3SS/Exploitation_models.git
-rhdir/git_Bio3SS_Exploitation_models: ; $(rhmake)
-rhdir/git_Bio3SS_Competition_models: url=https://github.com/Bio3SS/Competition_models.git
-rhdir/git_Bio3SS_Competition_models: ; $(rhmake)
-rhdir/git_Bio3SS_Structured_models: url=https://github.com/Bio3SS/Structured_models.git
-rhdir/git_Bio3SS_Structured_models: ; $(rhmake)
-rhdir/git_Bio3SS_Age_distributions: url=https://github.com/Bio3SS/Age_distributions.git
-rhdir/git_Bio3SS_Age_distributions: ; $(rhmake)
-rhdir/git_Bio3SS_Life_history: url=https://github.com/Bio3SS/Life_history.git
-rhdir/git_Bio3SS_Life_history: ; $(rhmake)
-rhdir/git_Bio3SS_Life_tables: url=https://github.com/Bio3SS/Life_tables.git
-rhdir/git_Bio3SS_Life_tables: ; $(rhmake)
-rhdir/git_Bio3SS_Compensation_models: url=https://github.com/Bio3SS/Compensation_models.git
-rhdir/git_Bio3SS_Compensation_models: ; $(rhmake)
-rhdir/git_Bio3SS_Birth_death_models: url=https://github.com/Bio3SS/Birth_death_models.git
-rhdir/git_Bio3SS_Birth_death_models: ; $(rhmake)
-rhdir/git_Bio3SS_Exponential_figures: url=https://github.com/Bio3SS/Exponential_figures.git
-rhdir/git_Bio3SS_Exponential_figures: ; $(rhmake)
-rhdir/git_Bio3SS_Population_time_series: url=https://github.com/Bio3SS/Population_time_series.git
-rhdir/git_Bio3SS_Population_time_series: ; $(rhmake)
-rhdir/git_Bio3SS_Evaluation_materials: url=https://github.com/Bio3SS/Evaluation_materials.git
-rhdir/git_Bio3SS_Evaluation_materials: ; $(rhmake)
-rhdir/git_Bio3SS_Homework: url=https://github.com/Bio3SS/Homework.git
-rhdir/git_Bio3SS_Homework: ; $(rhmake)
-rhdir/git_Bio3SS_Lectures: url=https://github.com/Bio3SS/Lectures.git
-rhdir/git_Bio3SS_Lectures: ; $(rhmake)
-rhdir/git_Bio3SS_submodLectures: url=https://github.com/Bio3SS/submodLectures.git
-rhdir/git_Bio3SS_submodLectures: ; $(rhmake)
-rhdir/git_Bio3SS_Lecture_images: url=https://github.com/Bio3SS/Lecture_images.git
-rhdir/git_Bio3SS_Lecture_images: ; $(rhmake)
-rhdir/git_Bio3SS_Grading_scripts: url=https://github.com/Bio3SS/Grading_scripts.git
-rhdir/git_Bio3SS_Grading_scripts: ; $(rhmake)
-rhdir/git_Bio3SS_Bio3SS_content: url=https://github.com/Bio3SS/Bio3SS_content.git
-rhdir/git_Bio3SS_Bio3SS_content: ; $(rhmake)
-rhdir/git_Bio3SS_bio3ss3: url=https://github.com/Bio3SS/bio3ss3.git
-rhdir/git_Bio3SS_bio3ss3: ; $(rhmake)
-rhdir/git_Bio3SS_Teaching_team: url=https://github.com/Bio3SS/Teaching_team.git
-rhdir/git_Bio3SS_Teaching_team: ; $(rhmake)
-rhdir/cron: url=https://github.com/dushoff/cron.git
-rhdir/cron: ; $(rhmake)
-rhdir/git_fishforwish_aging: url=https://github.com/fishforwish/aging.git
-rhdir/git_fishforwish_aging: ; $(rhmake)
-rhdir/git_CYGUBICKO_proview: url=https://github.com/CYGUBICKO/proview.git
-rhdir/git_CYGUBICKO_proview: ; $(rhmake)
-rhdir/git_wzmli_glasgow: url=https://github.com/wzmli/glasgow.git
-rhdir/git_wzmli_glasgow: ; $(rhmake)
-rhdir/Generation_distributions: url=https://github.com/dushoff/Generation_distributions.git
-rhdir/Generation_distributions: ; $(rhmake)
-rhdir/Endemic_curves: url=https://github.com/dushoff/Endemic_curves.git
-rhdir/Endemic_curves: ; $(rhmake)
-rhdir/git_mac-theobio_Disease_data: url=https://github.com/mac-theobio/Disease_data.git
-rhdir/git_mac-theobio_Disease_data: ; $(rhmake)
-rhdir/Generation_talks: url=https://github.com/dushoff/Generation_talks.git
-rhdir/Generation_talks: ; $(rhmake)
-rhdir/SIR_model_family: url=https://github.com/dushoff/SIR_model_family.git
-rhdir/SIR_model_family: ; $(rhmake)
-rhdir/git_Outbreak-analysis_WA_Ebola_Outbreak: url=https://github.com/Outbreak-analysis/WA_Ebola_Outbreak.git
-rhdir/git_Outbreak-analysis_WA_Ebola_Outbreak: ; $(rhmake)
-rhdir/SIR_simulations: url=https://github.com/dushoff/SIR_simulations.git
-rhdir/SIR_simulations: ; $(rhmake)
-rhdir/ss_pix: url=https://github.com/dushoff/ss_pix.git
-rhdir/ss_pix: ; $(rhmake)
-rhdir/git_ICI3D_rabies2019: url=https://github.com/ICI3D/rabies2019.git
-rhdir/git_ICI3D_rabies2019: ; $(rhmake)
-rhdir/git_davidearn_LBoM: url=https://github.com/davidearn/LBoM.git
-rhdir/git_davidearn_LBoM: ; $(rhmake)
-rhdir/git_davidearn_onmort: url=https://github.com/davidearn/onmort.git
-rhdir/git_davidearn_onmort: ; $(rhmake)
