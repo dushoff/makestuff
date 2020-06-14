e844b610a6ce87f87a9e31839a26b9a089c389b9
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-06-13 20:50:13 -0400

    Improve alltex logic?

--- texdeps.mk
+++ texdeps.mk
@@ -58,9 +58,10 @@ endif
 	$(MAKE) -f $< -f Makefile .texdeps/$*.out | tee .texdeps/$*.make.log 2>&1
 
 ## Try harder (and try to stop in the right place)
+## Fiddling with this 2020 Jun 13 (Sat)
 %.alltex: 
-	- $(MAKE) $*.deps
-	$(MAKE) $*.pdf || ($(MAKE) $*.deps && $(MAKE) $*.pdf)
+	$(MAKE) $*.deps || $(MAKE) $*.ltx
+	$(MAKE) $*.deps && $(MAKE) $*.pdf
 
 Ignore += .texdeps/
 
