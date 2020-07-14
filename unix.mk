9cfe885fd356f91d0d1298131c72e885fa11023f
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-07-14 12:11:16 -0400

    amsync

--- unix.mk
+++ unix.mk
@@ -16,6 +16,7 @@ CPF = /bin/cp -f
 CPR = /bin/cp -rf
 DIFF = diff
 GVEDIT = ($(VEDIT) $@ || gedit $@ || (echo ERROR: No editor found makestuff/unix.mk && echo set shell VEDIT variable && exit 1))
+GVEDIT = nano $@
 RMR = /bin/rm -rf
 LS = /bin/ls
 LN = /bin/ln -s
