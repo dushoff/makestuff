8e8b00d3310e0477061c01f8b106ced99f10cfc2
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-08-13 21:39:55 -0400

    yushan.mk

--- nodes.mk
+++ nodes.mk
@@ -1,7 +1,4 @@
 
-## Move this to a cluster-specific file, maybe?
-nodes = n01 n02 n03 n04 n05
-
 %.makejobs: /proc/uptime
 	- ssh $* "cd $(CURDIR); echo $* > $@;  ps aux | grep make | grep -v grep >> $@"
 
