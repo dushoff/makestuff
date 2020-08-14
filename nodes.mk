2972ac7c85d1270617ba223e1964719ac8504e56
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-08-13 21:56:17 -0400

    amsync

--- nodes.mk
+++ nodes.mk
@@ -4,16 +4,17 @@
 
 node_makejobs = $(nodes:%=%.makejobs)
 
-Ignore += nodes.makejobs
+Ignore += *.makejobs
 nodes.makejobs: $(node_makejobs)
 	$(cat)
 
 %.remotetarget:
 	ssh -f $* "cd $(CURDIR) && $(MAKE) $*_target > $*.remotelog" 
 
+Ignore += *.remotelog
 define remotemake
 %.$(1): 
-	ssh -f $(1) "cd $(CURDIR) && $(MAKE) $$* > $$*.$(1).log" 
+	ssh -f $(1) "cd $(CURDIR) && $(MAKE) $$* > $$*.$(1).remotelog" 
 endef
 
 $(foreach node,$(nodes),$(eval $(call remotemake,$(node))))
