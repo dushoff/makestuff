a943dddcb1f70d2d3cebe1d6de57e44bb8d4f805
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-12-12 23:23:55 -0500

    pre-DAIDD reboot

--- forms.mk
+++ forms.mk
@@ -19,7 +19,10 @@ text.pdf: text.txt
 	pdfroff $< | cpdf -crop "0.9in 10.8in 0.9in 0.2in" -stdin -o $@ 
 
 name.pdf: name.txt
-	pdfroff $< | cpdf -crop "0.9in 10.8in 0.9in 0.2in" -stdin -o $@ 
+	pdfroff $< | cpdf -crop "0.9in 10.8in 1.40in 0.25in" -stdin -o $@ 
+
+email.pdf: email.txt
+	pdfroff $< | cpdf -crop "0.9in 10.8in 1.40in 0.25in" -stdin -o $@ 
 
 date.pdf: date.txt
 	pdfroff $< | cpdf -crop "0.9in 10.8in 0.9in 0.2in" -stdin -o $@ 
@@ -74,4 +77,47 @@ sig.%.pdf: sig.%.jpg
 %.ppmed.png: %.pdf
 	convert -density 400x400 $< $@
 
--include makestuff/wrapR/pdf.mk
+.PRECIOUS: %-0.pdf
+%-0.pdf: %.pdf
+	pdfjam  $< 1 --outfile $@
+
+.PRECIOUS: %-1.pdf
+%-1.pdf: %.pdf
+	pdfjam  $< 2 --outfile $@
+
+.PRECIOUS: %-2.pdf
+%-2.pdf: %.pdf
+	pdfjam  $< 3 --outfile $@
+
+.PRECIOUS: %-3.pdf
+%-3.pdf: %.pdf
+	pdfjam  $< 4 --outfile $@
+
+.PRECIOUS: %-4.pdf
+%-4.pdf: %.pdf
+	pdfjam  $< 5 --outfile $@
+
+.PRECIOUS: %-5.pdf
+%-5.pdf: %.pdf
+	pdfjam  $< 6 --outfile $@
+
+.PRECIOUS: %-6.pdf
+%-6.pdf: %.pdf
+	pdfjam  $< 7 --outfile $@
+
+.PRECIOUS: %-7.pdf
+%-7.pdf: %.pdf
+	pdfjam  $< 8 --outfile $@
+
+.PRECIOUS: %-8.pdf
+%-8.pdf: %.pdf
+	pdfjam  $< 9 --outfile $@
+
+.PRECIOUS: %-9.pdf
+%-9.pdf: %.pdf
+	pdfjam  $< 10 --outfile $@
+
+.PRECIOUS: %.page.pdf
+%.page.pdf: %.Rout.pdf
+	pdfnup --outfile $@ --nup '2x2' $<
+ 
