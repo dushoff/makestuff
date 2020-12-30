f3ef6551ed8e69b6028ec5af43bc463b4b3cb307
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-12-30 00:25:20 -0500

    Good night

---
+++ rpkg.mk
@@ -0,0 +1,38 @@
+## Much of this is cribbed from McMasterPandemic and glmmTMB
+
+R = R --vanilla
+PACKAGE = rRlinks
+VERSION := $(PACKAGE)_$(shell sed -n '/^Version: /s///p' ./DESCRIPTION)
+TARBALL := $(VERSION).tar.gz
+## VERSION.var:
+
+Ignore += *.built
+
+pkgall: clean docs.built names.built pkgcheck.built
+
+docs.built: R/*.R
+	echo "suppressWarnings(roxygen2::roxygenize(\".\",roclets = c(\"collate\", \"rd\")))" | $(R)
+	touch $@
+
+names.built: R/*.R
+	echo "(roxygen2::roxygenize('.',roclets = 'namespace'))" | $(R)
+	touch $@
+
+pkgtest.built: $(TARBALL)
+	echo "devtools::test('.')" | $(R)
+	touch $@
+
+pkgcheck.built: install.built
+	echo "devtools::check('.')" | $(R)
+	touch $@
+
+clean:
+	find . \( -name "\.#*" -o -name "*~" -o -name ".Rhistory" \) -exec rm {} \;
+
+$(TARBALL): NAMESPACE $(wildcard R/*.*)
+	$(R) CMD build .
+
+install.built: $(TARBALL)
+	export NOT_CRAN=true; $(R) CMD INSTALL --preclean $<
+	@touch $@
+
