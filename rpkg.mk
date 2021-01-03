d2e0a26c91650f8bab11fbcd8e39d9d443e9568d
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-01-03 18:55:36 -0500

    some progress

--- rpkg.mk
+++ rpkg.mk
@@ -1,7 +1,11 @@
 ## Much of this is cribbed from McMasterPandemic and glmmTMB
 
+######################################################################
+
+## Names and variables
+
 R = R --vanilla
-PACKAGE = rRlinks
+PACKAGE = $(shell sed -n '/^Package: /s///p' ./DESCRIPTION)
 VERSION := $(PACKAGE)_$(shell sed -n '/^Version: /s///p' ./DESCRIPTION)
 TARBALL := $(VERSION).tar.gz
 
@@ -9,13 +13,16 @@ Ignore += $(TARBALL)
 
 ## VERSION.var:
 
-Ignore += *.built
+######################################################################
 
-pkgall: clean docs.built names.built pkgcheck.built
+## Shortcuts
 
-docs.built: $(wildcard R/*.R)
-	echo "suppressWarnings(roxygen2::roxygenize(\".\",roclets = c(\"collate\", \"rd\")))" | $(R)
-	touch $@
+pkgall: clean rpkgbuild/docs rpkgbuild/names rpkgbuild/pkgcheck
+dinst: rpkgbuild/quick
+
+######################################################################
+
+## Startup
 
 .Rbuildignore: 
 	cp ../.Rbuildignore $@ || touch $@
@@ -23,26 +30,45 @@ docs.built: $(wildcard R/*.R)
 DESCRIPTION: 
 	cp ../rpkgDescription $@ || touch $@
 
-NAMESPACE: names.built ;
-names.built: $(wildcard R/*.R)
-	echo "(roxygen2::roxygenize('.',roclets = 'namespace'))" | $(R)
-	touch $@
+######################################################################
 
-pkgtest.built: $(TARBALL)
-	echo "devtools::test('.')" | $(R)
-	touch $@
+## Tracking directory
 
-pkgcheck.built: install.built
-	echo "devtools::check('.')" | $(R)
-	touch $@
+Ignore += rpkgbuild/
+rpkgbuild:
+	$(mkdir)
 
-clean:
-	find . \( -name "\.#*" -o -name "*~" -o -name ".Rhistory" \) -exec rm {} \;
+######################################################################
+
+rpkgbuild/docs: $(wildcard R/*.R)
+	echo "suppressWarnings(roxygen2::roxygenize(\".\",roclets = c(\"collate\", \"rd\")))" | $(R)
+	touch $@
 
 $(TARBALL): NAMESPACE $(wildcard R/*.*)
 	$(R) CMD build .
 
-install.built: $(TARBALL)
+NAMESPACE: rpkgbuild/names ;
+rpkgbuild/names: $(wildcard R/*.R)
+	$(MAKE) rpkgbuild
+	echo "(roxygen2::roxygenize('.',roclets = 'namespace'))" | $(R)
+	touch $@
+
+rpkgbuild/pkgtest: $(TARBALL)
+	echo "devtools::test('.')" | $(R)
+	touch $@
+
+rpkgbuild/pkgcheck: rpkgbuild/install
+	echo "devtools::check('.')" | $(R)
+	touch $@
+
+rpkgbuild/install: $(TARBALL)
 	export NOT_CRAN=true; $(R) CMD INSTALL --preclean $<
 	@touch $@
+	@touch rpkgbuild/quick
 
+rpkgbuild/quick: NAMESPACE $(wildcard R/*.*)
+	R CMD INSTALL .
+	@touch $@
+
+clean:
+	find . \( -name "\.#*" -o -name "*~" -o -name ".Rhistory" \) -exec rm {} \;
