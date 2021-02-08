ba25922222d9b27c17d3cafcdad7c273d391a46e
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-02-07 22:56:49 -0500

    fiddling with mkfiles

--- mkfiles.mk
+++ mkfiles.mk
@@ -18,8 +18,8 @@ mklink = cd $* && $(LN) ../mkfiles/$*.make Makefile
 	$(MAKE) mkfiles/$*.make
 	$(mklink)
 
-%/Makefile: mkfiles/%.make
-	$(mklink)
+## When did I do this? Why? It seems aggressive
+## %/Makefile: mkfiles/%.make; $(mklink)
 
 ## Make Makefile a repository file
 ## Keep any changes made before that (remember to change Source and so one)
