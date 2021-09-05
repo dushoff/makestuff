69a5760d78ce85948da34e68c80dfa1d5e39363d
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-09-04 22:40:01 -0400

    try to sleep

--- mdyam.mk
+++ mdyam.mk
@@ -1,7 +1,7 @@
 ## Treat up to the first blank line as yaml
 Ignore += *.rym *.rwm
 rym_r = perl -nE "last if /^$$/; print; END{say}" $< > $*.rym
-rwm_r = Rscript -e 'library("rmarkdown"); render("$<", output_format="md_document", output_file="$*.rwm")'
+rwm_r = Rscript -e 'library("rmarkdown"); render("$<", output_format="md_document", output_file="$*.rwm")' $^
 
 Ignore += *.rmk
 %.rmk: %.rmd
