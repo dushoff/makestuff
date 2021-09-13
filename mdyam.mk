d102325c86bc2af84d791bfcf2b9f8745fe71938
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-09-12 22:40:17 -0400

    dmdmk

--- mdyam.mk
+++ mdyam.mk
@@ -3,13 +3,6 @@ Ignore += *.rym *.rwm
 rym_r = perl -nE "last if /^$$/; print; END{say}" $< > $*.rym
 rwm_r = Rscript -e 'library("rmarkdown"); render("$<", output_format="md_document", output_file="$*.rwm")' $^
 
-Ignore += *.rmk
-%.rmk: %.rmd
-	$(rym_r)
-	$(rwm_r)
-	$(CAT) $*.rym $*.rwm > $@
-	$(RM) $*.rym $*.rwm
-
 ## Works with dmdeps.mk; bad form to let dmd and rmd compete
 Ignore += *.rmk
 %.rmk: %.dmd
@@ -19,3 +12,7 @@ Ignore += *.rmk
 	$(CAT) $*.rym $*.rwm > $@
 	$(RM) $*.rym $*.rwm
 
+## Eventually want to make slides from rmk, but it's not working prettily now, so we need this parallel track
+%.rmd: %.dmd
+	$(dmd)
+	$(copy)
