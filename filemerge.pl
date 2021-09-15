4ca8a7f3053d1d6d717b8ec97f0edf64a32c108d
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-09-14 21:45:54 -0400

    pdf notes; fancy merge stuff

--- filemerge.pl
+++ filemerge.pl
@@ -18,16 +18,18 @@ while(<LS>)
 ## filename should be the first "word" thing on the line, and should have a .
 ## Directories are confusing me now
 ## Use a single quote to "escape" for files not in target directory
+## Try to remove a the first markdown [] tag (not looking for ! yet) 2021 Sep 14 (Tue)
 while(<>)
 {
 	last if /$untrack_string/;
 	chomp;
 	s/MISSING[^:]*: //;
+	say;
+	s/\[[^[]*\]\(//;
 	if(my ($fn) = m|^[\s*>#"*]*([\w/.]+\.\w+)|){
 		s/[^\s#*]/MISSING: $&/ unless defined $ls{$fn};
 		$ls{$fn} = 1;
 	}
-	say;
 }
 
 ## Print out things not noted as present
