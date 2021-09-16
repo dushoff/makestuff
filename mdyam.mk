2b33f4a3303a52f0b1d0dad720f0903572828bcd
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-09-15 21:32:31 -0400

    working on pipeline

--- mdyam.mk
+++ mdyam.mk
@@ -3,16 +3,15 @@ Ignore += *.rym *.rwm
 rym_r = perl -nE "last if /^$$/; print; END{say}" $< > $*.rym
 rwm_r = Rscript -e 'library("rmarkdown"); render("$<", output_format="md_document", output_file="$*.rwm")' $^
 
-## Works with dmdeps.mk; bad form to let dmd and rmd compete
 Ignore += *.rmk
-%.rmk: %.dmd
-	$(dmd_r)
+%.rmk: %.rmd
 	$(rym_r)
 	$(rwm_r)
 	$(CAT) $*.rym $*.rwm > $@
 	$(RM) $*.rym $*.rwm
 
-## Eventually want to make slides from rmk, but it's not working prettily now, so we need this parallel track
+## Deprecate this; maybe change them to rmd and manually invoke dmd_r somehow
+## rmd is now made from dmd; don't let both be sources 2021 Sep 15 (Wed) 
 %.rmd: %.dmd
 	$(dmd_r)
 	$(copy)
