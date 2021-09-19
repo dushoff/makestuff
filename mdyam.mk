377272a7a62496a09307725f984410752ed0c760
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-09-19 15:03:00 -0400

    define-ing

--- mdyam.mk
+++ mdyam.mk
@@ -3,12 +3,16 @@ Ignore += *.rym *.rwm
 rym_r = perl -nE "last if /^$$/; print; END{say}" $< > $*.rym
 rwm_r = Rscript -e 'library("rmarkdown"); render("$<", output_format="md_document", output_file="$*.rwm")' $^
 
-Ignore += *.rmk
-%.rmk: %.rmd
+define rmk_r
 	$(rym_r)
 	$(rwm_r)
 	$(CAT) $*.rym $*.rwm > $@
 	$(RM) $*.rym $*.rwm
+endef
+
+Ignore += *.rmk
+%.rmk: %.rmd
+	$(rmk_r)
 
 ## Deprecate this; maybe change them to rmd and manually invoke dmd_r somehow
 ## rmd is now made from dmd; don't let both be sources 2021 Sep 15 (Wed) 
