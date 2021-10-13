3aa7bf13bafcbbe33f819d80848e4c6de6ae3fd1
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-10-13 18:42:40 -0400

    mediadir rules

--- docmedia.mk
+++ docmedia.mk
@@ -4,6 +4,8 @@ Ignore += word media
 	$(RMR) $@
 	mv word/media/ $@
 
+## This will remake forever, which seems OK since media is a temporary place.
+## Better solution would be to change the pointers to <foo>.media in the pipeline
 %.mediadir: %.media
 	$(RM) media
 	ln -fs $< media
