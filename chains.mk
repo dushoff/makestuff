0d43c87a36011918bcdd96c5da94692ed7e774e7
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-01-11 21:19:36 -0500

    chains dev

--- chains.mk
+++ chains.mk
@@ -7,3 +7,12 @@ $(foreach pat,$(recipeChain),\
 		$(eval $(call $(pat)_r,$(s)))\
 	)\
 )
+
+script_r = %.$(2).$(3).Rout: $(2).R $(1) ; $$(pipeR)
+$(foreach pat,$(scriptChain),\
+	$(foreach s, $($(pat)),\
+		$(eval $(call script_r,$($(pat)_dep),$(s),$(pat)))\
+	)\
+)
+
+pipeRimplicit += $(recipeChain) $(scriptChain)
