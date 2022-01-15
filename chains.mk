2e2b9e880086fe8cba1284b5de53c3699a98ef72
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-01-15 14:03:58 -0500

    Gen II not looking good

--- chains.mk
+++ chains.mk
@@ -1,15 +1,17 @@
 
+## Gen 1 families
+
 $(foreach pat,$(recipeChain),\
 	$(foreach s, $($(pat)),\
 		$(eval $(call $(pat)_r,$(s)))\
 	)\
 )
 
+## scriptChain
 script_r = %.$(2).$(3).Rout: $(2).R $(1) ; $$(pipeR)
 $(foreach pat,$(scriptChain),\
 	$(foreach s, $($(pat)),\
 		$(eval $(call script_r,$($(pat)_dep),$(s),$(pat)))\
-		$(info $(call script_r,$($(pat)_dep),$(s),$(pat)))\
 	)\
 )
 
@@ -22,3 +24,44 @@ $(foreach pat,$(parseChain),\
 )
 
 pipeRimplicit += $(recipeChain) $(scriptChain) $(parseChain)
+
+######################################################################
+
+## Gen 1 families
+
+$(foreach pat,$(recipeChain),\
+	$(foreach s, $($(pat)),\
+		$(eval $(call $(pat)_r,$(s)))\
+	)\
+)
+
+## scriptChain
+script_r = %.$(2).$(3).Rout: $(2).R $(1) ; $$(pipeR)
+$(foreach pat,$(scriptChain),\
+	$(foreach s, $($(pat)),\
+		$(eval $(call script_r,$($(pat)_dep),$(s),$(pat)))\
+	)\
+)
+
+## parseChain
+parse_r = %.$(2).$(3).Rout: $(3).R $(1) ; $$(pipeR)
+$(foreach pat,$(parseChain),\
+	$(foreach s, $($(pat)),\
+		$(eval $(call parse_r,$($(pat)_dep),$(s),$(pat)))\
+	)\
+)
+
+pipeRimplicit += $(recipeChain) $(scriptChain) $(parseChain)
+
+######################################################################
+
+## Gen 2 families
+## Call recipes with family; example; dep
+
+## scriptStep
+scriptStep_r = %.$(2).$(1).Rout: $(2).R $(3) ; $$(pipeR)
+scriptStep_longr = %.$(2).$(1).Rout: $(2).R $(3) ; $$(pipeR)
+
+pipeRimplicit += $(scriptStep) $(scriptLink)
+
+######################################################################
