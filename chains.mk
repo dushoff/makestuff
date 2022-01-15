3d73f7e8f9a06c8245e37ed9461203b23ff4d85f
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-01-15 11:29:16 -0500

    working on spline extrapolation

--- chains.mk
+++ chains.mk
@@ -9,11 +9,12 @@ script_r = %.$(2).$(3).Rout: $(2).R $(1) ; $$(pipeR)
 $(foreach pat,$(scriptChain),\
 	$(foreach s, $($(pat)),\
 		$(eval $(call script_r,$($(pat)_dep),$(s),$(pat)))\
+		$(info $(call script_r,$($(pat)_dep),$(s),$(pat)))\
 	)\
 )
 
+## parseChain
 parse_r = %.$(2).$(3).Rout: $(3).R $(1) ; $$(pipeR)
-parse_rr = %.$(2).$(3).Rout: $(3).R $(1) ; date
 $(foreach pat,$(parseChain),\
 	$(foreach s, $($(pat)),\
 		$(eval $(call parse_r,$($(pat)_dep),$(s),$(pat)))\
