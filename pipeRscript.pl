12538f182b7b53a8a2d71c6a7173df96e90554db
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-11-28 18:10:08 +0000

    arrive Glasgow

--- pipeRscript.pl
+++ pipeRscript.pl
@@ -3,7 +3,10 @@ use 5.10.0;
 
 while (<>){
 	chomp;
-	next unless /(R --vanilla.*rtmp)/;
+	next unless (
+		/(R --vanilla.*rtmp)/
+		or /Rscript --vanilla.*render/
+	);
 	my $com=$1;
 	$com =~ s/rtmp/Rout/;
 	say $com;
