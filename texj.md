e18bbb9c8b3895d3239a0fc52f8cbc4e4ee134b7
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-07-18 15:56:17 -0400

    tff??

---
+++ texj.md
@@ -0,0 +1,3 @@
+It would be good to make the final main target have the canonical name, doc.pdf: doc.tex
+
+But there are problems. We want to make the 
--- texj.mk
+++ texj.mk
@@ -1,51 +1,31 @@
-latex ?= pdflatex
-non ?= -interaction=nonstopmode
-job = -jobname=$(@:%.pdf=%)
-
-run = $(latex) $(job) $<
-forcerun = $(latexnon) $(non) $(job) $<
-
+latexEngine ?= pdflatex
+latexNonstop ?= -interaction=nonstopmode
+latexJob = -jobname=$(basename $@)
 bibtex ?= biber $* || bibtex $*
 
-## Draft version; just make it. Will sometimes report an error even when a pdf is made; this should be fixed maybe.
-%.tex.pdf: %.tex
-	$(run) || $(forcerun)
+runLatex = $(latexEngine) $(latexJob) $(basename $<)
+RUNLatex = $(latexEngine) $(latexNonstop) $(latexJob) (basename $<)
 
-## .pdf is never up to date (makedeps is fake)
-## Why is extra makedeps needed? Implicit rule recursion is confusing
-%.pdf: %.tex %.tex.deps %.makedeps makedeps
-	$(MAKE) $*.deps.pdf
+%.aux: %.tex
+	$(runLatex)
+	$(MV) $*.pdf $*.aux.pdf
 
-## Working on work flow choices 2021 Oct 20 (Wed)
-## Using latexonly to jump to tex error; touch to keep make chain going
-## But .deps.pdf can be up to date (if .tex.deps didn't need anything)
-%.deps.pdf: %.tex %.tex.deps
-	$(texir) || ($(latexonly) && touch $<)
-	$(CP) $@ $*.pdf 
+## This .pdf should never be up to date
+## because Makefile can't evaluate whether the deps are up to date
+%.pdf: phony
+	$(MAKE) -f $*.tex.mk -f Makefile $*.tex.deps
+	$(MAKE) $*.aux
+	$(CP) $*.aux.pdf $@
 
-## This rule meant to be over-ridden by rules in the corresponding .mk
-## Helps with dependency logic above
-.PRECIOUS: %.tex.deps
-%.tex.deps:
-	touch $@
+%.bbl: %.aux 
+	$(rm)
+	$(bibtex)
 
-# A phony target (this is how .tex.deps is "really" made)
-%.makedeps: %.tex.mk %.tex
-	$(MAKE) -f $< -f Makefile $*.subdeps
-	$(MAKE) -f $< -f Makefile $*.tex.deps
-
-## Must not exist!
-makedeps: ;
+phony: ;
 
 .PRECIOUS: %.tex.mk
 %.tex.mk: %.tex 
 	perl -wf makestuff/texi.pl $< > $@
-
-## Include logic is still a bit tangled (sad face)
-
-## We need ugly logic here because texi doesn't respond to changes in .bib
-%.bbl: %.tex %.tex.pdf $(wildcard *.bib)
-	($(bibtex)) || ($(RM) $@ && false)
 
 texfiles = $(wildcard *.tex)
 Ignore += $(texfiles:tex=pdf)
@@ -57,15 +37,3 @@ Ignore += *.nav *.snm *.toc
 Ignore += *.run.xml
 Ignore += *.tex.* *.subdeps *.makedeps
 Ignore += *.deps.pdf *.deps.out
-
-### Doesn't quite fit here (or anywhere)
-
-ifndef PUSH
--include makestuff/perl.def
-endif
-
-Ignore += $(wildcard *_olddiff.*)
-.PRECIOUS: %_olddiff.tex
-%_olddiff.tex: %.tex.*.oldfile %.tex makestuff/latexdiff.pl
-	$(PUSH)
-
