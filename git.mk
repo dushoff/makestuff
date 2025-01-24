be65216805cfd483aa54700af56cdffb0e0abb13
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-01-24 17:31:43 +0800

    Leaving CPH

--- git.mk
+++ git.mk
@@ -67,7 +67,7 @@ $(pardirs):
 
 Ignore += up.time all.time
 up.time: commit.time
-	$(MAKE) pullup
+	$(MAKE) pull
 	$(MAKE) pushup
 	touch $@
 
@@ -159,6 +159,7 @@ push.%: commit.time
 ## without adding to all pulls; maybe not useful?
 ## 2022 Aug 05 (Fri) added submodule incantation
 ## 2023 Jan 29 (Sun) subtracted submodule incantation; add it manually to submodule directories
+## 2025 Jan 24 (Fri) pullup is used now for rclone; use it for manual pulls but not for pulls that are required just to push
 
 pullup: pull
 
