08e88bf71ab7800f820573e3ccaebaddbad4e22f
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-02-27 14:19:26 -0500

    Leaving office

--- git.mk
+++ git.mk
@@ -9,6 +9,10 @@
 
 ## USE github_private or github_public to make a repo named after directory
 github_%: | .git commit.time
+	gh repo create --$* --source . --push
+
+## More flexible version?? Doesn't match origin somehow. What is origin?
+ghrepo_%: | .git commit.time
 	gh repo create $(repoName) --$* --source=. --remote=upstream --push
 
 ######################################################################
