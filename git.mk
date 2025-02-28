8b84ab8468c370803cda5f4fb269094cf0364979
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-02-28 10:54:29 -0500

    new file:   gh.mk

--- git.mk
+++ git.mk
@@ -4,22 +4,11 @@
 
 ######################################################################
 
-## gh repo create --public mac-theobio/makeIntro2025 --source . --push
-
-initBranch ?= main
 .git:
-	git init -b $(initBranch)
+	git init
 
 ## USE github_private or github_public to make a repo named after directory
 github_%: | .git commit.time
-	gh repo create --$* --source=. --push
-
-## USE github_private or github_public to make a repo named after directory
-github_%: | .git commit.time
-	gh repo create --$* --source . --push
-
-## More flexible version?? Doesn't match origin somehow. What is origin?
-ghrepo_%: | .git commit.time
 	gh repo create $(repoName) --$* --source=. --remote=upstream --push
 
 ######################################################################
@@ -618,3 +607,4 @@ Ignore += *.blame
 
 store_all:
 	git config --global credential.helper 'store'
+
