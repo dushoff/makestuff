679026582b2e6411a60ee6caef696a54d934b0a1
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-07-24 20:07:43 -0400

    fiVe

--- ignore.pl
+++ ignore.pl
@@ -2,7 +2,18 @@ use strict;
 use 5.10.0;
 use Env;
 
-say "## content from makestuff";
+my $delim = "## content from makestuff";
+my $head = "## edited content";
+
+if (open(my $fh, '<', '.gitignore')) {
+	while (my $line = <$fh>) {
+		last if /$delim/;
+		say $head if $.==1;
+	}
+	close $fh;
+}
+
+say $delim;
 my $ignore = $ENV{Ignore};
 $ignore =~ s/\s+/\n/g;
 say $ignore;
